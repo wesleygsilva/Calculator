36d4c6745e513bd7dd2e749297419e4e8c80c66b
Wesley Gomes <gomes.wesley@outlook.com.br>
2022-03-19 14:45:02 -0300

    Mapping numbers, actions and operations

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+//Operations' Results
+const boxResult = document.querySelector('#box-result');
+const input  = document.querySelector('#input');
+
+//Actions
+const clearEntry  = document.querySelector('#clear-entry');
+const clear  = document.querySelector('#clear');
+const equal  = document.querySelector('#equal');
+const del  = document.querySelector('#delete');
+
+//Operations
+const percent = document.querySelector('#percent');
+const sum  = document.querySelector('#sum');
+const subtract  = document.querySelector('#subtract');
+const multiply  = document.querySelector('#multiply');
+const divide  = document.querySelector('#divide');
+
+//Numbers
+const btn0  = document.querySelector('#btn0');
+const btn1  = document.querySelector('#btn1');
+const btn2  = document.querySelector('#btn2');
+const btn3  = document.querySelector('#btn3');
+const btn4  = document.querySelector('#btn4');
+const btn5  = document.querySelector('#btn5');
+const btn6  = document.querySelector('#btn6');
+const btn7  = document.querySelector('#btn7');
+const btn8  = document.querySelector('#btn8');
+const btn9  = document.querySelector('#btn9');
+const virgula  = document.querySelector('#virgula');
+
+
+
