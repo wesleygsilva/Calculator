12fc82dca5b8ed1f4c8d6d0ceb734bb3f3e286ac
Wesley Gomes <gomes.wesley@outlook.com.br>
2022-03-21 20:47:32 -0300

    Code refactory

--- src/js/script.js
+++ src/js/script.js
@@ -28,7 +28,8 @@ const btn8  = document.querySelector('#btn8');
 const btn9  = document.querySelector('#btn9');
 const virgula  = document.querySelector('#virgula');
 
-const getValue = value => {
+//Get digited values (only numbers and virgula)
+const getValue = value => {  
     if (value == ',') {
         if (input.value.indexOf(',') == -1) {
             input.value = input.value + ',';
@@ -40,15 +41,18 @@ const getValue = value => {
     }
 };
 
+//Clear the input display
 const clearDisplay = () => {
     input.value = 0;
 };
 
+//Clear all displays
 const clearAll = () => {
     input.value = 0;
     boxResult.value = '';
 };
 
+//Event Listeners
 clear.addEventListener("click", e => {
     clearAll();
 });
@@ -62,184 +66,250 @@ btn0.addEventListener("click",  e => {
 });
 
 btn1.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 btn2.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 btn3.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 btn4.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 btn5.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 btn6.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 btn7.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 btn8.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 btn9.addEventListener("click",  e => {
+    restartOperation();
     getValue(e.target.value);
 });
 
 virgula.addEventListener("click",  e => {
     getValue(e.target.value);
 });
+//Event Listeners
 
+//Change value , by . to calc values
 const replaceValues = (value) => {
+    if (value == '') {
+        value = 0;
+    }
+
     return value.toString().replace(',', '.');
 }
 
+//Return just 2 decimal places
 const roundToTwo = (number) => {
     return +(Math.round(number + "e+2")  + "e-2");
 }
 
-const execSum = (value1, value2) => {
-    let firstValue = replaceValues(value1);
-    let secondValue = replaceValues(value2);
-
-    let result = parseFloat(firstValue) + parseFloat(secondValue);
-    return (roundToTwo(result)).toString().replace(".", ",");
-};
-
-const execSubtract = (value1, value2) => {
-    let firstValue = replaceValues(value1);
-    let secondValue = replaceValues(value2);
-
-    let result = parseFloat(firstValue) - parseFloat(secondValue);
-    return (roundToTwo(result)).toString().replace(".", ",");
-};
-
-const execMultiply = (value1, value2) => {
-    let firstValue = replaceValues(value1);
-    let secondValue = replaceValues(value2);
-
-    let result = parseFloat(firstValue) * parseFloat(secondValue);
-    return (roundToTwo(result)).toString().replace(".", ",");
-};
-
-const execDivide = (value1, value2) => {
-    if (value2 === 0) {
-        return `Cannot divide by zero`;
-    } else {
-        let firstValue = replaceValues(value1);
-        let secondValue = replaceValues(value2);
-
-        let result = parseFloat(firstValue) / parseFloat(secondValue);
-        return (roundToTwo(result)).toString().replace(".", ",");
-    }
-};
-
+//Main function to proccess values
 const execOperation = (getInfoCalc) => {
+    let firstValue = replaceValues(getInfoCalc.firstValue);
+    let secondValue = replaceValues(getInfoCalc.secondValue);
+
+    let result = 0;
+
     switch (getInfoCalc.operator) {
         case '+':
-            return execSum(getInfoCalc.firstValue, getInfoCalc.secondValue); 
+            result = parseFloat(firstValue) + parseFloat(secondValue);
+            break;
         case '-':
-            return execSubtract(getInfoCalc.firstValue, getInfoCalc.secondValue);  
+            result = parseFloat(firstValue) - parseFloat(secondValue);
+            break;
         case 'x':
-            return execMultiply(getInfoCalc.firstValue, getInfoCalc.secondValue);  
+            result = parseFloat(firstValue) * parseFloat(secondValue);
+            break;
         case '/':
-            return execDivide(getInfoCalc.firstValue, getInfoCalc.secondValue); 
+            if (parseFloat(secondValue) == 0) {
+                return `Cannot divide by zero`;
+            } else {
+                result = parseFloat(firstValue) / parseFloat(secondValue);
+            } 
+            break;
         case '%':
             return '';
         default:
-            console.log('Erro: Operação inválida');
+            '';
     }
+
+    return (roundToTwo(result)).toString().replace(".", ",");
 };
 
-const getOperation = operation => {
+//Calc percent value
+const setPercentValue = () => {
+    if (boxResult.value == '') {
+        boxResult.value = 0;
+    }
+    input.value = roundToTwo((parseFloat(boxResult.value) / 100) * parseFloat(input.value)).toString().replace(".", ",");
+};
+
+//put info to calc values
+const setValuesInfoCalc = (operator, firstValue, secondValue) => {
     let getInfoCalc = {
         operator: '',
         firstValue: 0,  
         secondValue: 0
     }
+    
+    getInfoCalc.operator    = operator;
+    getInfoCalc.firstValue  = firstValue;
+    getInfoCalc.secondValue = secondValue;
 
-    if (operation == '=') {
-        if (boxResult.value !== '') {
-            let operator = boxResult.value[boxResult.value.length - 1];
-            let array = ['+', '-', 'x', '/', '%'];
-            let index = array.indexOf(operator);
+    return getInfoCalc;
+};
 
-            if ((index !== -1)) {
-                getInfoCalc.operator    = operator;
-                getInfoCalc.firstValue  = boxResult.value.substring(0, boxResult.value.indexOf(' '));
-                getInfoCalc.secondValue = input.value;
-                
-                boxResult.value = execOperation(getInfoCalc);
-                clearDisplay();
-            }
-        }
-    } else {  
-        let operator = boxResult.value[boxResult.value.length - 1];
-        //routine for exec operation before the main operation
-        if ((operator !== undefined) && (operator !== operation)) {
-            let array = ['+', '-', 'x', '/', '%'];
-            let index = array.indexOf(operator);
+//Return index of array with operations
+const returnIndex = () => {
+    let operator = boxResult.value[boxResult.value.length - 1];
+    let array = ['+', '-', 'x', '/', '%'];
+    let index = array.indexOf(operator);
 
-            if ((index !== -1)) {
-                getInfoCalc.operator    = operator;
-                getInfoCalc.firstValue  = boxResult.value.substring(0, boxResult.value.indexOf(' '));
-                getInfoCalc.secondValue = input.value;
+    return index;
+};
+
+//Clean all display case boxResult <> '' and input.value = 0
+const restartOperation = () => {
+    if ((boxResult.value !== '') && (input.value == '0')) { 
+        if ((returnIndex() == -1)) {
+            input.value = '';
+            boxResult.value = '';  
+        }   
+    }
+}
+
+//Calcs equal calc
+const setEqualOperation = (operator) => {
+    if (boxResult.value !== '') {
+        if ((returnIndex() !== -1)) {
+
+            let getInfoCalc = setValuesInfoCalc(operator, 
+                                                boxResult.value.substring(0, boxResult.value.indexOf(' ')),
+                                                input.value);
             
-                getInfoCalc.operator = operator;
-                let firstValue = boxResult.value.substring(0, boxResult.value.indexOf(' ')); 
-
-                if (firstValue !== '') {
-                    getInfoCalc.firstValue = firstValue;
-                } else if (boxResult.value !== '') {
-                    getInfoCalc.firstValue = boxResult.value;    
-                }
-        
-                if (input.value !== '0') {
-                    getInfoCalc.secondValue = input.value;   
-                }
-
-                boxResult.value = execOperation(getInfoCalc);
-
-                if ((operation == 'x') || (operation == '/')) {
-                    input.value = 1;        
-                } else {
-                    clearDisplay();   
-                }
-            } else {
-                boxResult.value = `${boxResult.value} ${operation}`;  
-                return false; 
-            }
+            boxResult.value = execOperation(getInfoCalc);
+            clearDisplay();
         }
-
-        getInfoCalc.operator = operation;
-        let firstValue = boxResult.value.substring(0, boxResult.value.indexOf(' ')); 
-
-        if (firstValue !== '') {
-            getInfoCalc.firstValue = firstValue;
-        } else if (boxResult.value !== '') {
-            getInfoCalc.firstValue = boxResult.value;    
-        } else if ((operation == 'x') || (operation == '/')) {
-            getInfoCalc.firstValue = 1;
-        }
-
-        getInfoCalc.secondValue = input.value;   
-
-        boxResult.value = `${execOperation(getInfoCalc)} ${operation}`;
-        clearDisplay();
     }
 };
 
+//Validations about infoCalc object
+const prepareInfoCalc = (operation) => {
+    let getInfoCalc = setValuesInfoCalc(operation, 
+                                        boxResult.value.substring(0, boxResult.value.indexOf(' ')),
+                                        input.value);
+
+    if (getInfoCalc.firstValue !== '') {
+        getInfoCalc.secondValue = input.value; 
+    } else if (boxResult.value !== '') {
+        getInfoCalc.firstValue = boxResult.value; 
+        getInfoCalc.secondValue = input.value;    
+    } else if (operation == 'x') {
+        getInfoCalc.firstValue = 1;
+        getInfoCalc.secondValue = input.value; 
+    } else if (operation == '/') {
+        getInfoCalc.secondValue = 1;
+        getInfoCalc.firstValue = input.value;
+    } else if ((operation == '-') && (boxResult.value == '')) {
+        input.value = input.value * -1;
+        getInfoCalc.secondValue = input.value; 
+    } else {
+        getInfoCalc.secondValue = input.value; 
+    }
+
+    return getInfoCalc;
+};
+
+//calcs normal operations
+const setValueOperation = (operation) => {
+    let operator = boxResult.value[boxResult.value.length - 1];
+
+    if ((operator !== undefined) && (operator !== operation)) {
+        if ((returnIndex() !== -1)) {
+            let getInfoCalc = setValuesInfoCalc(operator, 
+                                                boxResult.value.substring(0, boxResult.value.indexOf(' ')),
+                                                input.value);
+            
+            if (boxResult.value !== '') {
+                getInfoCalc.firstValue = boxResult.value;    
+            }
+    
+            if (input.value !== '0') {
+                getInfoCalc.secondValue = input.value;   
+            }
+
+            boxResult.value = execOperation(getInfoCalc);
+
+            if ((operation == 'x') || (operation == '/')) {
+                input.value = 1;        
+            } else {
+                clearDisplay();   
+            }
+        } else {
+            boxResult.value = `${boxResult.value} ${operation}`;  
+            return false; 
+        }
+    }
+
+    let getInfoCalc = prepareInfoCalc(operation);
+
+    boxResult.value = `${execOperation(getInfoCalc)} ${operation}`;
+    clearDisplay();
+};
+
+//Prepare operation to calc values
+const getOperation = operation => {
+    try {
+        if (operation == '=') {
+            setEqualOperation(boxResult.value[boxResult.value.length - 1]);    
+        } else if (operation == '%') {
+            setPercentValue(operation); 
+        } else {  
+            setValueOperation(operation);        
+        }
+    } catch(error) {
+        alert(error) 
+    }
+};
+
+//Delete just 1 char of input display
+const deleteChar = (value) => {
+    if (input.value !== '0') {
+        input.value = input.value.slice(0, -1);
+    }
+
+    if (input.value == '') {
+        input.value = 0;
+    }
+}
+
+//Event Listeners
 sum.addEventListener("click", e => {
     getOperation(e.target.value);
 });
@@ -264,20 +334,90 @@ equal.addEventListener("click", e => {
     getOperation(e.target.value);
 });
 
-const deleteChar = (value) => {
-    if (input.value !== '0') {
-        input.value = input.value.slice(0, -1);
-    }
-
-    if (input.value == '') {
-        input.value = 0;
-    }
-}
-
 del.addEventListener("click", e => {
     deleteChar(e.target.value);
 });
+//Event Listeners
 
+//Keyboard Listeners
+document.addEventListener("keydown", e => {
+    switch (e.keyCode) {
+        case 46:
+            clearEntry.click();
+            break;
+        case 8:
+            del.click();
+            break;
+
+        case 111:
+            divide.click();
+            break;
+        case 106:
+            multiply.click();
+            break;
+        case 109:
+            subtract.click();
+            break;
+        case 107:
+            sum.click();
+            break;
+        case 110:
+            virgula.click();
+            break;
+        case 194:
+            virgula.click();
+            break;
+        case 13:
+            equal.click();
+            break;
+        default:
+            '';
+    }
+
+    if (e.shiftKey && e.which === 53) { 
+        percent.click();
+    }
+});
+
+document.addEventListener("keypress", e => {
+    switch (e.keyCode) {
+        case 48:
+            btn0.click();
+            break;
+        case 49:
+            btn1.click();
+            break;
+        case 50:
+            btn2.click();
+            break;
+        case 51:
+            btn3.click();
+            break;
+        case 52:
+            btn4.click();
+            break;
+        case 53:
+            btn5.click();
+            break;
+        case 54:
+            btn6.click();
+            break;
+        case 55:
+            btn7.click();
+            break;
+        case 56:
+            btn8.click();
+            break;
+        case 57:
+            btn9.click();
+            break;
+        default:
+            '';
+    }
+});
+//Keyboard Listeners
+
+//start cleanning display
 const init = () => {
     clearDisplay();
 };
